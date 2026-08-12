fd576574e456a8c72df8046982e35a614a4a4047
OscarPorter <oscardipee@gmail.com>
2026-08-12 17:24:10 +0100

    Boilerplate and API key added.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ.get("SPOTIFY_API_KEY")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
