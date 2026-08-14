c98e667cb6cc78b5fd85081f586dd663d11969fc
OscarPorter <oscardipee@gmail.com>
2026-08-14 17:44:19 +0100

    Spotipy Test

--- app.py
+++ app.py
@@ -3,9 +3,32 @@ from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.environ.get("SPOTIFY_API_KEY")
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+load_dotenv()
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+sp = spotipy.Spotify(
+   auth_manager=SpotifyOAuth(
+      client_id=CLIENT_ID,
+      client_secret=CLIENT_SECRET,
+      redirect_uri="http://127.0.0.1:5000",
+      scope="user-library-read"))
+
+taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
+results = sp.artist_albums(taylor_uri, album_type='album')
+albums = results['items']
+while results['next']:
+   results = sp.next(results)
+   albums.extend(results['items'])
+
+for album in albums:
+   print(album['name'])
+
+    
+'''
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,3 +37,4 @@ def home():
 
 if __name__ == '__main__':
    app.run()
+'''
